cfeaea5c1796067389977d9ae0a48e8c6897c7c6
ricardo <ricardotvera.54@gmail.com>
2020-09-10 21:32:53 -0700

    separated dom from main

--- scr/main.js
+++ scr/main.js
@@ -1,4 +1,3 @@
-
 const playerTurn = document.getElementById('turn');
 
 const Gameboard = () => {
@@ -75,44 +74,10 @@ game.tcell.forEach(cell => {
       turn();
     }
   });
-});
-
 const btn1 = document.getElementById('btn-1');
 const btn2 = document.getElementById('btn-2');
 const firstForm = document.getElementById('form-1');
 const secondForm = document.getElementById('form-2');
 const restart = document.getElementById('restart');
-
-
-btn1.addEventListener('click', (event) => {
-  event.preventDefault();
-  if (document.getElementById('player-1').value === '') {
-    const pl = document.getElementById('player-1');
-    pl.setAttribute('placeholder', 'wrong name');
-  } else {
-    player1.getName = document.getElementById('player-1').value;
-    firstForm.style.display = 'none';
-    secondForm.style.display = 'block';
-    playerTurn.textContent = `Player ${player1.getName} turn`;
-  }
+adddbtn(btn1, btn2, restart, firstForm, secondForm)
 });
-
-btn2.addEventListener('click', (event) => {
-  event.preventDefault();
-  if (document.getElementById('player-2').value === '' || document.getElementById('player-2').value === player1.getName) {
-    const pl = document.getElementById('player-2');
-    pl.setAttribute('placeholder', 'wrong name');
-  } else {
-    player2.getName = document.getElementById('player-2').value;
-    secondForm.style.display = 'none';
-    const board = document.getElementById('board');
-    board.style.display = 'block';
-    restart.style.display = 'block';
-  }
-});
-
-
-restart.addEventListener('click', () => {
-  window.location.reload();
-});
-
