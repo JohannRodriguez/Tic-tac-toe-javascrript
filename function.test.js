3530a52711a1290ec30998be4d210fecf822015a
Lilith <themrblacksapce@hotmail.com>
2020-10-23 09:26:30 -0600

    Correct ends

--- function.test.js
+++ function.test.js
@@ -11,29 +11,29 @@ test('Gets gameboard with cells', () => {
 });
 
 test('says the turn correctly', () => {
-  const play = document.createElement("h1")
-  play.setAttribute('id', 'turn')
-  play.textContent = 'Player mono turn'
+  const play = document.createElement("h1");
+  play.setAttribute('id', 'turn');
+  play.textContent = 'Player mono turn';
 
-  const make = document.createElement("h1")
-  make.setAttribute('id', 'turn')
-  make.textContent = 'Player Fiat turn'
+  const make = document.createElement("h1");
+  make.setAttribute('id', 'turn');
+  make.textContent = 'Player Fiat turn';
 
   expect(turn({getName:"Fiat", getPlays:[1, 2, 3]}, {getName:"mono", getPlays:[5, 6]}, play)).toEqual(make);
 });
 
 test('we have a winner', () => {
 
-  const play = document.createElement("h1")
-  play.setAttribute('id', 'turn')
-  play.textContent = 'Player Fiat turn'
+  const play = document.createElement("h1");
+  play.setAttribute('id', 'turn');
+  play.textContent = 'Player Fiat turn';
 
   const game = {
     winCondition : [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
     [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-  }
+  };
 
-  const player = {getName:"Fiat", getPlays:[1, 2, 3]}
+  const player = {getName:"Fiat", getPlays:[1, 2, 3]};
 
   expect(gameFlow(player.getPlays, play, {getName:"Fiat", getPlays:[1, 2, 3]}, {getName:"mono", getPlays:[5, 6]})).toEqual({"playerWinner": "Fiat is the winner!"});
-})
+});
